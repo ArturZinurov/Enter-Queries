e7ccd29347427f4446c313f4856d38ea80e96ec2
Artur Zinurov <arturzinurov971015@gmail.com>
2018-11-12 14:30:12 -0700

    added public and scr

--- Tests/theBigOne.js
+++ Tests/theBigOne.js
@@ -7,3 +7,15 @@ var modifyWantedPageObject = {}
 var cancelTestData = require('../test-assets/cTestData')
 var cancelFunc = require('../test-assets/cancelWantedFunc')
 var cancelWantedPageObjects = {}
+
+module.exports = {
+    beforeEach: browser => {
+        enterWanted = browser.page.enterWantedPageObjects()
+        enterWanted.navigate()
+        modifyWantedPageObject = browser.page.modifyWantedPageObjects()
+        modifyWantedPageObject.navigate()
+        cancelWantedPageObjects = browser.page.cancelWantedPageObjects()
+        cancelWantedPageObjects.navigate()
+
+    }
+}
